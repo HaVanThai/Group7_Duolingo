e9ead67b793af699ddefe47a5a252d22d73d482b
Thais <thaihvse04801@fpt.edu.vn>
2018-10-21 13:19:14 +0700

    Create 3 fragments

--- app/src/main/java/com/group7/duolingo/MainActivity.java
+++ app/src/main/java/com/group7/duolingo/MainActivity.java
@@ -3,30 +3,40 @@ package com.group7.duolingo;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
-import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-//    private TextView mTextMessage;
+    // Initial all fragments
+    final Fragment fragment1 = new StudyFragment();
+    final Fragment fragment2 = new ProfileFragment();
+    final Fragment fragment3 = new SettingsFragment();
+    final FragmentManager fm = getSupportFragmentManager();
+    Fragment active = fragment1;
+
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
 
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-//            switch (item.getItemId()) {
-//                case R.id.navigation_home:
-//                    mTextMessage.setText(R.string.title_home);
-//                    return true;
-//                case R.id.navigation_dashboard:
-//                    mTextMessage.setText(R.string.title_dashboard);
-//                    return true;
-//                case R.id.navigation_notifications:
-//                    mTextMessage.setText(R.string.title_notifications);
-//                    return true;
-//            }
+            switch (item.getItemId()) {
+                case R.id.navigation_study:
+                    fm.beginTransaction().hide(active).show(fragment1).commit();
+                    active = fragment1;
+                    return true;
+                case R.id.navigation_profile:
+                    fm.beginTransaction().hide(active).show(fragment2).commit();
+                    active = fragment2;
+                    return true;
+                case R.id.navigation_settings:
+                    fm.beginTransaction().hide(active).show(fragment3).commit();
+                    active = fragment3;
+                    return true;
+            }
             return false;
         }
     };
@@ -36,9 +46,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-//        mTextMessage = (TextView) findViewById(R.id.message);
-//        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
-//        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        fm.beginTransaction().add(R.id.main_content, fragment3, "3").hide(fragment3).commit();
+        fm.beginTransaction().add(R.id.main_content, fragment2, "2").hide(fragment2).commit();
+        fm.beginTransaction().add(R.id.main_content,fragment1, "1").commit();
+
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
 
 }
