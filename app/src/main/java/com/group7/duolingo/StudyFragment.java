031fa7796831b392ece64a91ddf7a85716cb682a
Thais <thaihvse04801@fpt.edu.vn>
2018-10-22 21:15:12 +0700

    update lesson's icon interface

--- app/src/main/java/com/group7/duolingo/StudyFragment.java
+++ app/src/main/java/com/group7/duolingo/StudyFragment.java
@@ -14,6 +14,8 @@ import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+
 import utils.DpiUtils;
 
 
@@ -22,9 +24,16 @@ import utils.DpiUtils;
  */
 public class StudyFragment extends Fragment {
 
+    ArrayList<String> lessons = new ArrayList<String>();
 
     public StudyFragment() {
         // Required empty public constructor
+
+        lessons.add("Lesson 1");
+        lessons.add("Lesson 2");
+        lessons.add("Lesson 3");
+        lessons.add("Lesson 4");
+        lessons.add("Lesson 5");
     }
 
 
@@ -37,74 +46,90 @@ public class StudyFragment extends Fragment {
         // Root LinearLayout in ScrollView
         LinearLayout scrollViewContainer = view.findViewById(R.id.scroll_view_container);
 
-        // Row LinearLayout in scrollViewContainer
-        LinearLayout rowLayout = new LinearLayout(getContext());
-        rowLayout.setLayoutParams(new LinearLayout.LayoutParams(
-                LinearLayout.LayoutParams.MATCH_PARENT,
-                LinearLayout.LayoutParams.WRAP_CONTENT
-        ));
-        rowLayout.setOrientation(LinearLayout.HORIZONTAL);
-        rowLayout.setGravity(Gravity.CENTER_HORIZONTAL);
+        int index = 0;
+        int rowSize = 1;
+        int itemLayoutPadding = DpiUtils.toPixels(20, getResources().getDisplayMetrics());
+        int cardViewPadding = DpiUtils.toPixels(10, getResources().getDisplayMetrics());
+        int parentCardViewRadius = DpiUtils.toPixels(40, getResources().getDisplayMetrics());
+        int childCardViewRadius = DpiUtils.toPixels(30, getResources().getDisplayMetrics());
+        int imageViewSize = DpiUtils.toPixels(40, getResources().getDisplayMetrics());
+        while (index < lessons.size()) {
 
-        // Item LinearLayout in rowLayout
-        LinearLayout itemLayout = new LinearLayout(getContext());
-        itemLayout.setLayoutParams(new LinearLayout.LayoutParams(
-                LinearLayout.LayoutParams.WRAP_CONTENT,
-                LinearLayout.LayoutParams.WRAP_CONTENT
-        ));
-        itemLayout.setOrientation(LinearLayout.VERTICAL);
-        itemLayout.setGravity(Gravity.CENTER_HORIZONTAL);
+            // Row LinearLayout in scrollViewContainer
+            LinearLayout rowLayout = new LinearLayout(getContext());
+            rowLayout.setLayoutParams(new LinearLayout.LayoutParams(
+                    LinearLayout.LayoutParams.MATCH_PARENT,
+                    LinearLayout.LayoutParams.WRAP_CONTENT
+            ));
+            rowLayout.setOrientation(LinearLayout.HORIZONTAL);
+            rowLayout.setGravity(Gravity.CENTER_HORIZONTAL);
 
-        // Parent CardView
-        CardView parentCardView = new CardView(getContext());
-        parentCardView.setLayoutParams(new LinearLayout.LayoutParams(
-                DpiUtils.toPixels(80, getResources().getDisplayMetrics()),
-                DpiUtils.toPixels(80, getResources().getDisplayMetrics())
-        ));
-        parentCardView.setRadius(DpiUtils.toPixels(40, getResources().getDisplayMetrics()));
-        parentCardView.setCardBackgroundColor(getResources().getColor(R.color.colorLightGray));
-        int paddingDpi = DpiUtils.toPixels(10, getResources().getDisplayMetrics());
-        parentCardView.setContentPadding(paddingDpi, paddingDpi, paddingDpi, paddingDpi);
+            if(index == 0 || index == lessons.size() - 1) {
+                rowSize = 1;
+            } else {
+                rowSize = 2;
+            }
+            for(int i = 0; i < rowSize && index < lessons.size(); i++) {
+                // Item LinearLayout in rowLayout
+                LinearLayout itemLayout = new LinearLayout(getContext());
+                itemLayout.setLayoutParams(new LinearLayout.LayoutParams(
+                        LinearLayout.LayoutParams.WRAP_CONTENT,
+                        LinearLayout.LayoutParams.WRAP_CONTENT
+                ));
+                itemLayout.setOrientation(LinearLayout.VERTICAL);
+                itemLayout.setGravity(Gravity.CENTER_HORIZONTAL);
+                itemLayout.setPadding(itemLayoutPadding, itemLayoutPadding, itemLayoutPadding, 0);
 
-        // Child CardView
-        CardView childCardView = new CardView(getContext());
-        childCardView.setLayoutParams(new LinearLayout.LayoutParams(
-                DpiUtils.toPixels(60, getResources().getDisplayMetrics()),
-                DpiUtils.toPixels(60, getResources().getDisplayMetrics())
-        ));
-        childCardView.setRadius(DpiUtils.toPixels(30, getResources().getDisplayMetrics()));
-        childCardView.setCardBackgroundColor(getResources().getColor(R.color.colorBlue));
-        childCardView.setContentPadding(paddingDpi, paddingDpi, paddingDpi, paddingDpi);
+                // Parent CardView
+                CardView parentCardView = new CardView(getContext());
+                parentCardView.setLayoutParams(new LinearLayout.LayoutParams(
+                        2*parentCardViewRadius,
+                        2*parentCardViewRadius
+                ));
+                parentCardView.setRadius(parentCardViewRadius);
+                parentCardView.setCardBackgroundColor(getResources().getColor(R.color.colorLightGray));
+                parentCardView.setContentPadding(cardViewPadding, cardViewPadding, cardViewPadding, cardViewPadding);
 
-        // ImageView inside child CardView
-        ImageView imageView = new ImageView(getContext());
-        imageView.setLayoutParams(new LinearLayout.LayoutParams(
-                DpiUtils.toPixels(40, getResources().getDisplayMetrics()),
-                DpiUtils.toPixels(40, getResources().getDisplayMetrics())
-        ));
-        imageView.setScaleType(ImageView.ScaleType.CENTER_INSIDE);
-        imageView.setImageResource(getResources().getIdentifier("ant", "drawable", getContext().getPackageName()));
+                // Child CardView
+                CardView childCardView = new CardView(getContext());
+                childCardView.setLayoutParams(new LinearLayout.LayoutParams(
+                        2 * childCardViewRadius,
+                        2 * childCardViewRadius
+                ));
+                childCardView.setRadius(childCardViewRadius);
+                childCardView.setCardBackgroundColor(getResources().getColor(R.color.colorBlue));
+                childCardView.setContentPadding(cardViewPadding, cardViewPadding, cardViewPadding, cardViewPadding);
 
-        // TextView inside Item LinearLayout
-        TextView textView = new TextView(getContext());
-        textView.setLayoutParams(new LinearLayout.LayoutParams(
-                LinearLayout.LayoutParams.WRAP_CONTENT,
-                LinearLayout.LayoutParams.WRAP_CONTENT
-        ));
-        textView.setTextSize(TypedValue.COMPLEX_UNIT_SP, 24);
-        textView.setText("First Lesson");
+                // ImageView inside child CardView
+                ImageView imageView = new ImageView(getContext());
+                imageView.setLayoutParams(new LinearLayout.LayoutParams(
+                        imageViewSize,
+                        imageViewSize
+                ));
+                imageView.setScaleType(ImageView.ScaleType.CENTER_INSIDE);
+                imageView.setImageResource(getResources().getIdentifier("ant", "drawable", getContext().getPackageName()));
 
-        // Constraints
-        childCardView.addView(imageView);
-        parentCardView.addView(childCardView);
+                // TextView inside Item LinearLayout
+                TextView textView = new TextView(getContext());
+                textView.setLayoutParams(new LinearLayout.LayoutParams(
+                        LinearLayout.LayoutParams.WRAP_CONTENT,
+                        LinearLayout.LayoutParams.WRAP_CONTENT
+                ));
+                textView.setTextSize(TypedValue.COMPLEX_UNIT_SP, 24);
+                textView.setText(lessons.get(index++));
 
-        itemLayout.addView(parentCardView);
-        itemLayout.addView(textView);
+                // Constraints
+                childCardView.addView(imageView);
+                parentCardView.addView(childCardView);
 
-        rowLayout.addView(itemLayout);
+                itemLayout.addView(parentCardView);
+                itemLayout.addView(textView);
 
-        scrollViewContainer.addView(rowLayout);
+                rowLayout.addView(itemLayout);
+            }
 
+            scrollViewContainer.addView(rowLayout);
+        }
         return view;
     }
 
